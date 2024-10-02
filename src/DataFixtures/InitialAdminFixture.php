160dda5be32909c971975d90b107e51c6c92f0a0
AliciaOC <96443722+AliciaOC@users.noreply.github.com>
2024-10-02 20:49:15 +0200

    primer admin añadido a la bbdd y ya es posible iniciar sesión con él

---
+++ src/DataFixtures/InitialAdminFixture.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class InitialAdminFixture extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+    private ContainerBagInterface $params;
+
+    public function __construct(
+        UserPasswordHasherInterface $passwordHasher,
+        ContainerBagInterface $params
+    ) {
+        $this->passwordHasher = $passwordHasher;
+        $this->params = $params;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $usuarioAdmin = new User();
+        $usuarioAdmin->setEmail('admin@admin.com');
+        $usuarioAdmin->setRoles(['ROLE_ADMIN']);
+        $usuarioAdmin->setVerified(true);
+
+        $password = $this->params->get('app.super_admin_password');
+
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $usuarioAdmin,
+            $password
+        );
+        $usuarioAdmin->setPassword($hashedPassword);
+
+        $manager->persist($usuarioAdmin);
+        $manager->flush();
+    }
+}

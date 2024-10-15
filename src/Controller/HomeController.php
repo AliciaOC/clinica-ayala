15595e517dd093698c30804921bcd3894f9821af
AliciaOC <ortegacastroalicia@gmail.com>
2024-10-15 21:09:47 +0200

    añadido dinamismo al parrafo que acompaña al enlace de inicio

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $peticion, EntityManagerInterface $entity): Response
     {
+        $nombrePerfil="Inicio de Sesión";
+
+        if($this->getUser())
+        {
+            $userRol=$this->getUser()->getRoles();
+            if(in_array('ROLE_ADMIN', $userRol)){
+                $nombrePerfil="Perfil: Admin";
+            }elseif(in_array('ROLE_TERAPEUTA', $userRol)){
+                $nombrePerfil="Perfil: Terapeuta";
+            }elseif(in_array('ROLE_Cliente', $userRol)){
+                $nombrePerfil="Perfil: Cliente";
+            }
+        }
         return $this->render('home/index.html.twig', [
             'perfil' => 'app_login_redirect',
+            'nombre' => $nombrePerfil
         ]);
     }
 }

ebc12f63b62717f40690148fdf4883b1a116a57e
AliciaOC <ortegacastroalicia@gmail.com>
2024-10-18 21:49:31 +0200

    refactorización en el registro de usuarios

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,14 +5,114 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Form\RegistrarAdminType;
+use App\Form\RegistrarTerapeutaType;
+use Exception;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
-        return $this->render('admin/index.html.twig', [
+        return $this->render('admin/indexAdmin.html.twig', [
             'controller_name' => 'AdminController',
+        ]);
+    }
+
+    #[Route('/admin/registro-admin', name: 'app_admin_registrar')]
+    public function registrarAdmin(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrarAdminType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //introduzco los valores que faltan para crear un administrador (el email va en el formulario)
+            //la contraseña para nuevos usuarios es el string de su email que hay antes del @
+            $email = $user->getEmail();
+            $passwordProvisonal = substr($email, 0, strpos($email, '@')); //el 0 es el inicio de la cadena, y strpos busca la primera aparición de @
+            $passwordHashed = $userPasswordHasher->hashPassword($user, $passwordProvisonal);
+            $user->setPassword($passwordHashed);
+            $user->setRoles(['ROLE_ADMIN']);
+            $user->setNuevo(true);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('app_admin');
+        }
+
+        return $this->render('admin/adminRegistro.html.twig', [
+            'registroForm' => $form,
+        ]);
+    }
+
+    public function crearUser(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, string $rol)
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrarAdminType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //introduzco los valores que faltan para crear un administrador (el email va en el formulario)
+            //la contraseña para nuevos usuarios es el string de su email que hay antes del @
+            $email = $user->getEmail();
+            $passwordProvisonal = substr($email, 0, strpos($email, '@')); //el 0 es el inicio de la cadena, y strpos busca la primera aparición de @
+            $passwordHashed = $userPasswordHasher->hashPassword($user, $passwordProvisonal);
+            $user->setPassword($passwordHashed);
+            $user->setRoles([$rol]);
+            $user->setNuevo(true);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return true;
+            //return $this->redirectToRoute('app_admin');
+        }
+
+        
+        return $this->render('admin/adminRegistro.html.twig', [
+            'registroForm' => $form,
+        ]);
+        
+    }
+
+    public function registrarTerapeuta(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrarAdminType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //introduzco los valores que faltan para crear un administrador (el email va en el formulario)
+            //la contraseña para nuevos usuarios es el string de su email que hay antes del @
+            $email = $user->getEmail();
+            $passwordProvisonal = substr($email, 0, strpos($email, '@')); //el 0 es el inicio de la cadena, y strpos busca la primera aparición de @
+            $passwordHashed = $userPasswordHasher->hashPassword($user, $passwordProvisonal);
+            $user->setPassword($passwordHashed);
+            $user->setRoles(['ROLE_ADMIN']);
+            $user->setNuevo(true);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('app_admin');
+        }
+
+        return $this->render('admin/adminRegistro.html.twig', [
+            'registroForm' => $form,
         ]);
     }
 }
--- src/Form/RegistrarTerapeutaType.php
+++ src/Form/RegistrarTerapeutaType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\Terapeuta;
+use App\Entity\Tratamiento;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -11,26 +14,36 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 
-class RegistrarAdminType extends AbstractType
+class RegistrarTerapeutaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('titulacion')
+            ->add('nombre')
+            ->add('horario')
+            ->add('tratamientos', EntityType::class, [
+                'class' => Tratamiento::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+                'required' => false,
+            ])
             ->add('email', EmailType::class)
             ->add('confirmacion', CheckboxType::class, [
                 'mapped' => false, //no se mapea con la entidad
-                'label' => 'Informe al nuevo usuario que su contraseña provisional es lo que hay antes del @ en su email y que el programa le pedirá cambiar la contraseña en el primer acceso.',
+                'label' => 'Informe al nuevo Terapeuta que su contraseña provisional es lo que hay antes del @ en su email y que el programa le pedirá cambiar la contraseña en el primer acceso.',
                 'required' => true,
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Guardar',
             ]);
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            'data_class' => Terapeuta::class,
         ]);
     }
 }

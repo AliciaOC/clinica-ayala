2d1aac92272f492ca105fe963bf57ebc414a7180
AliciaOC <ortegacastroalicia@gmail.com>
2024-12-29 03:28:13 +0100

    mejoras en estilos de header y footer

--- assets/app.js
+++ assets/app.js
@@ -13,3 +13,20 @@ window.jQuery = $;
  */
 import './styles/app.scss';
 
+$(document).ready(function() {
+    // Función para actualizar la altura del relleno del footer cuando el ancho de la ventana es >= 1400px
+    function actualizarRellenoFooter() {
+        if ($(window).width() >= 1400) {  
+            let alturaFooter = $('footer').outerHeight();  
+            $('#relleno_footer').height(alturaFooter); 
+            $('#relleno_footer').css('margin-top', alturaFooter * -1);
+        }
+    }
+
+    actualizarRellenoFooter();
+
+    $(window).resize(function() {
+        actualizarRellenoFooter();
+    });
+});
+

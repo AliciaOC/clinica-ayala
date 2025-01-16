315d73b1355f0894b604e61ff137ecab46fc536f
AliciaOC <ortegacastroalicia@gmail.com>
2025-01-17 00:38:47 +0100

    refactorización

--- assets/js/hamburguesa.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    // Hamburguesa (mantenemos la funcionalidad en jQuery)
-    $("#hamburguesa-navbar").click(function () {
-      $(this).toggleClass("active");
-      $("#navbar").toggleClass("active");
-    });
-  });
